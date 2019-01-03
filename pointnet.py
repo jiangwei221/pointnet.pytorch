aa5d4fd334e053e86f1010cf63c6fd01d4563653
Wei Jiang <weijiang@Weis-MacBook-Pro.local>
2019-01-02 22:18:18 -0800

    update

--- pointnet.py
+++ pointnet.py
@@ -74,6 +74,8 @@ class PointNetfeat(nn.Module):
         mask = torch.ne(torch.max(x,1)[0], 0)
         mask = mask.unsqueeze(1).repeat(1, 1024, 1)
         mask.float()
+        import IPython
+        IPython.embed()
         mask[mask==0] = float('inf')
         mask[mask==1] = 0
         batchsize = x.size()[0]
